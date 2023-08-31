7a7b4f1078f186f28724d4b5ede01cb1bfe509c8
itsdawei <dhlee@usc.edu>
2023-08-31 11:10:38 -0700

    Update pylintrc

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import find_packages, setup
 
-with open("README.md") as readme_file:
+with open("README.md", encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
 # with open("HISTORY.md") as history_file:
