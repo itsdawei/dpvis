a03d2eb97e746687c6751161fdad4b66c0e9bf5b
Tianyu Wang <twang659@usc.edu>
2023-09-10 12:56:48 -0700

    slice tests

--- tests/dp_array_logger_int_test.py
+++ tests/dp_array_logger_int_test.py
@@ -78,34 +78,40 @@ def test_array_read_write_log_multiple_arrays():
     assert len(dp1.logger.logs) == 3
 
 
-# def test_array_slice_read_write_log():
-#     dp = DPArray(10)
-#     dp[0:5] = 1
-#     assert dp.logger.logs[0]["op"] == Operation.WRITE
-#     assert dp.logger.logs[0]["idx"] == {"dp_array": set([0, 1, 2, 3, 4])}
-#     assert len(dp.logger.logs) == 1
+def test_array_slice_read_write_log():
+    dp = DPArray(10)
+    dp[0:5] = 1
+    assert dp.logger.logs[0]["op"] == Op.WRITE
+    assert dp.logger.logs[0]["idx"] == {"dp_array": set([0, 1, 2, 3, 4])}
+    assert len(dp.logger.logs) == 1
 
-#     temp = dp[1:4]
-#     assert dp.logger.logs[0]["op"] == Operation.WRITE
-#     assert dp.logger.logs[0]["idx"] == {"dp_array": set([0, 1, 2, 3, 4])}
-#     assert dp.logger.logs[1]["op"] == Operation.READ
-#     assert dp.logger.logs[1]["idx"] == {"dp_array": set([1, 2, 3])}
-#     assert len(dp.logger.logs) == 2
+    temp = dp[1:4]
+    assert dp.logger.logs[0]["op"] == Op.WRITE
+    assert dp.logger.logs[0]["idx"] == {"dp_array": set([0, 1, 2, 3, 4])}
+    assert dp.logger.logs[1]["op"] == Op.READ
+    assert dp.logger.logs[1]["idx"] == {"dp_array": set([1, 2, 3])}
+    assert len(dp.logger.logs) == 2
 
-# def test_2d_array_slice_read_write_log():
-#     dp = DPArray((10, 10))
-#     dp[0:5, 0:5] = 1
-#     assert dp.logger.logs[0]["op"] == Operation.WRITE
-#     assert dp.logger.logs[0]["idx"] == {"dp_array": set([(i, j) for i in range(5) for j in range(5)])}
-#     assert len(dp.logger.logs) == 1
+def test_2d_array_slice_read_write_log():
+    dp = DPArray((10, 10))
 
-#     dp[::3, ::3] = 2
-#     assert dp.logger.logs[0]["op"] == Operation.WRITE
-#     assert dp.logger.logs[0]["idx"] == {"dp_array": set([(i, j) for i in range(5) for j in range(3)])}
+    dp[0:2, 0:2] = 1
+    assert dp.logger.logs[0]["op"] == Op.WRITE
+    assert dp.logger.logs[0]["idx"] == {"dp_array": set([(0, 0), (0, 1), (1, 0), (1, 1)])}
+    assert len(dp.logger.logs) == 1
 
-#     temp = dp[3::, 4:8]
-#     assert dp.logger.logs[0]["op"] == Operation.WRITE
-#     assert dp.logger.logs[0]["idx"] == {"dp_array": set([(i, j) for i in range(5) for j in range(5)])}
-#     assert dp.logger.logs[1]["op"] == Operation.READ
-#     assert dp.logger.logs[1]["idx"] == {"dp_array": set([(i, j) for i in range(3, 10) for j in range(4, 8)])}
-#     assert len(dp.logger.logs) == 2
+    dp[0:5, 0:5] = 1
+    assert dp.logger.logs[0]["op"] == Op.WRITE
+    assert dp.logger.logs[0]["idx"] == {"dp_array": set([(i, j) for i in range(5) for j in range(5)])}
+    assert len(dp.logger.logs) == 1
+
+    dp[::3, ::3] = 2
+    assert dp.logger.logs[0]["op"] == Op.WRITE
+    assert dp.logger.logs[0]["idx"] == {"dp_array": set([(i, j) for i in range(5) for j in range(3)])}
+
+    temp = dp[3::, 4:8]
+    assert dp.logger.logs[0]["op"] == Op.WRITE
+    assert dp.logger.logs[0]["idx"] == {"dp_array": set([(i, j) for i in range(5) for j in range(5)])}
+    assert dp.logger.logs[1]["op"] == Op.READ
+    assert dp.logger.logs[1]["idx"] == {"dp_array": set([(i, j) for i in range(3, 10) for j in range(4, 8)])}
+    assert len(dp.logger.logs) == 2
