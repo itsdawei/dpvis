d0c347185eb0fb5d281c5ea65d7fca398f1540c7
Aditya Prasad <aprasad4@usc.edu>
2023-10-05 22:31:41 -0700

    Fixed knapsack demo.

--- demos/knapsack.py
+++ demos/knapsack.py
@@ -5,11 +5,15 @@ import fire
 # An item is a 2-tuple with (space, value)
 # items is a list of items in the problem instance.
 def knapsack(items=[(2, 4), (4, 3), (7, 12), (5, 6), (13, 13)], capacity=14):
-    OPT = DPArray((len(items), capacity))
+    # Adding a filler element since python is 0-indexed
+    items.insert(0, (0, -1))
+
+    # Initialize DPArray
+    OPT = DPArray((len(items), capacity+1))
 
     # Recurrence: OPT(i, C) = max(OPT(i-1, C), OPT(i-1, C-i.space) + i.val)
     for idx, item in enumerate(items):
-        for rem in range(capacity):
+        for rem in range(capacity+1):
             # Base case: 0 value if there are no items left or if there is no space.
             if idx == 0 or rem == 0:
                 OPT[idx, rem] = 0
