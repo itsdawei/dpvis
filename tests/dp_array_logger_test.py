0b3cbd9c7a21e77c24b4e309f2d93edab48baa17
Tianyu Wang <twang659@usc.edu>
2023-10-07 13:14:57 -0700

    Support writing with slice (#18)
    ## Description
    
    logger writing is now enabled. some existing bugs with slice reading are
    also fixed (read with list of indices)
    
    ## TODO
    
    <!-- Notable points that this PR has either accomplished or will
    accomplish. -->
    
    - [x] fix read with list of indices bug
    - [x] support writing to logger
    
    ## TESTING
    
    - [x] added tests, uncommented logger write tests
    
    ## Questions
    
    ## Status
    
    - [x] I have read the guidelines in
    [CONTRIBUTING.md](https://github.com/itsdawei/dynamically_programmed/blob/master/CONTRIBUTING.md)
    - [x] I have formatted my code using `yapf`
    - [x] I have linted my code with `pylint`
    - [x] I have tested my code by running `pytest`
    - [x] I have added a one-line description of my change to the changelog
    in `HISTORY.md`
    - [x] This PR is ready to go
    
    ---------
    
    Co-authored-by: itsdawei <dhlee@usc.edu>

--- tests/dp_array_logger_test.py
+++ tests/dp_array_logger_test.py
@@ -1,5 +1,6 @@
 """Tests the interaction between DPArray and Logger."""
 import functools
+
 import numpy as np
 import pytest
 
@@ -285,46 +286,68 @@ def test_slice_reading(s):
     assert dp.logger.logs[1] == {"op": Op.READ, "idx": {"dp_array": truth}}
 
 
-def test_slice_assignment():
-    # currently do not support assignment through slices
+def test_slice_reading_list_of_indices():
+    dp = DPArray(10)
+    for i in range(10):
+        dp[i] = i**2
+
+    indices = [1, 2, 3]
+    _ = dp[indices]
+    truth = {i: None for i in indices}
+    assert dp.logger.logs[1] == {"op": Op.READ, "idx": {"dp_array": truth}}
+
+
+@pytest.mark.parametrize("s", [np.s_[::2], np.s_[:2], np.s_[4:], np.s_[:6], 5],
+                         ids=["a", "b", "c", "d", "e"])
+def test_slice_logging(s):
+    dp = DPArray(10)
+    dp[s] = 1
+    if isinstance(s, int):
+        s = np.s_[s:s + 1]
+    truth = {i: 1 for i in range(*s.indices(10))}
+    assert dp.logger.logs[0] == {"op": Op.WRITE, "idx": {"dp_array": truth}}
+    assert len(dp.logger.logs) == 1
+
+
+@pytest.mark.parametrize("slice_1",
+                         [np.s_[::2], np.s_[:2], np.s_[4:], np.s_[:6], 5],
+                         ids=["a", "b", "c", "d", "e"])
+@pytest.mark.parametrize("slice_2",
+                         [np.s_[::2], np.s_[:2], np.s_[4:], np.s_[:6], 1],
+                         ids=["a", "b", "c", "d", "e"])
+def test_2d_slice_logging(slice_1, slice_2):
+    dp = DPArray((10, 10))
+
+    dp[slice_1, slice_2] = 1
+    if isinstance(slice_1, int):
+        slice_1 = np.s_[slice_1:slice_1 + 1]
+    if isinstance(slice_2, int):
+        slice_2 = np.s_[slice_2:slice_2 + 1]
+    truth = {
+        (i, j): 1 for i in range(*slice_1.indices(10))
+        for j in range(*slice_2.indices(10))
+    }
+    assert dp.logger.logs[0] == {"op": Op.WRITE, "idx": {"dp_array": truth}}
+    assert len(dp.logger.logs) == 1
+
+
+def test_list_assignment():
     dp = DPArray(10)
 
-    with pytest.raises(ValueError):
-        dp[:2] = 1
+    dp[::2] = [1, 1, 1, 1, 1]
 
-
-# @pytest.mark.parametrize("s",
-#                          [np.s_[::2], np.s_[:2], np.s_[4:], np.s_[:6], 5],
-#                          ids=["a", "b", "c", "d", "e"])
-# def test_slice_logging(s):
-#     dp = DPArray(10)
-
-#     dp[s] = 1
-#     if isinstance(s, int):
-# s = np.s_[s:s + 1]
-#     truth = {i:1 for i in range(*s.indices(10))}
-#     assert dp.logger.logs[0] == {"op": Op.WRITE, "idx": {"dp_array": truth}}
-#     assert len(dp.logger.logs) == 1
-
-# @pytest.mark.parametrize("slice_1",
-#                          [np.s_[::2], np.s_[:2], np.s_[4:], np.s_[:6], 5],
-#                          ids=["a", "b", "c", "d", "e"])
-# @pytest.mark.parametrize("slice_2",
-#                          [np.s_[::2], np.s_[:2], np.s_[4:], np.s_[:6], 1],
-#                          ids=["a", "b", "c", "d", "e"])
-# def test_2d_slice_logging(slice_1, slice_2):
-#     dp = DPArray((10, 10))
-
-#     dp[slice_1, slice_2] = 1
-#     if isinstance(slice_1, int):
-#         slice_1 = np.s_[slice_1:slice_1 + 1]
-#     if isinstance(slice_2, int):
-#         slice_2 = np.s_[slice_2:slice_2 + 1]
-#     truth = {(i, j)
-#              for i in range(*slice_1.indices(10))
-#              for j in range(*slice_2.indices(10))}
-#     assert dp.logger.logs[0] == {"op": Op.WRITE, "idx": {"dp_array": truth}}
-#     assert len(dp.logger.logs) == 1
+    assert dp.logger.logs[0] == {
+        "op": Op.WRITE,
+        "idx": {
+            "dp_array": {
+                0: 1,
+                2: 1,
+                4: 1,
+                6: 1,
+                8: 1,
+            }
+        }
+    }
 
 
 def test_get_timesteps_one_array():
@@ -436,6 +459,7 @@ def test_get_timesteps_two_arrays():
         Op.HIGHLIGHT: set(),
     }.items()
 
+
 def test_to_timestep_2d():
     dp = DPArray((3, 3), "dp")
     dp[0, 0] = 1
@@ -443,9 +467,8 @@ def test_to_timestep_2d():
 
     timesteps = dp.get_timesteps()
     assert len(timesteps) == 1
-    assert np.all(timesteps[0]["dp"]["contents"] == [[1, None, None],
-                                                     [None, 2, None],
-                                                     [None, None, None]])
+    assert np.all(timesteps[0]["dp"]["contents"] ==
+                  [[1, None, None], [None, 2, None], [None, None, None]])
     assert timesteps[0]["dp"].items() >= {
         Op.READ: set(),
         Op.WRITE: {(0, 0), (1, 1)},
@@ -455,12 +478,10 @@ def test_to_timestep_2d():
     _ = dp[1, 1]
     timesteps1 = dp.get_timesteps()
     assert len(timesteps1) == 2
-    assert np.all(timesteps1[1]["dp"]["contents"] == [[1, None, None],
-                                                      [None, 2, None],
-                                                      [None, None, None]])
+    assert np.all(timesteps1[1]["dp"]["contents"] ==
+                  [[1, None, None], [None, 2, None], [None, None, None]])
     assert timesteps1[1]["dp"].items() >= {
         Op.READ: {(1, 1)},
         Op.WRITE: set(),
         Op.HIGHLIGHT: set(),
     }.items()
-    
