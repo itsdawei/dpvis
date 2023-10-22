1db27677b83ccae3bf107317ada6fd30b8cdbdba
Ramiro <rdeocamp@usc.edu>
2023-10-22 00:26:10 -0700

    added get backtrack and validate backtrack support

--- tests/dp_array_test.py
+++ tests/dp_array_test.py
@@ -231,6 +231,86 @@ def test_max_min_2d(max_min, r, max_val, min_val):
 
     assert dp.arr[h - 1, w - 1] == truth
 
+@pytest.mark.parametrize(
+        "matrix, solutions",
+        [(np.array([[0, 2, 1],
+                    [1, 0, 1],
+                    [1, 2, 1],]),
+                    ([(0, 0), (0, 1), (0, 2), (1, 2), (2, 2)],
+                     [(0, 0), (0, 1), (1, 1), (2, 1), (2, 2)],
+                     [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)],
+                     )
+                    )],
+        ids=["simple"]
+)
+def test_backtrack(matrix, solutions):
+    (h, w) = matrix.shape
+    dp = DPArray((h, w))
+
+    dp[0, 0] = matrix[0, 0]
+    for i in range(h):
+        for j in range(w):
+            indices = []
+            elements = []
+            if i == 0 and j == 0:
+                continue
+
+            if i > 0:
+                indices.append((i - 1, j))
+                elements.append(dp[i - 1, j])
+
+            if j > 0:
+                indices.append((i, j - 1))
+                elements.append(dp[i, j - 1])
+
+            dp[i, j] = dp.max(indices, elements) + matrix[i, j]
+
+    assert dp.backtrack() in solutions
+
+@pytest.mark.parametrize(
+        "matrix, solutions, not_solutions",
+        [(np.array([[0, 2, 1],
+                    [1, 0, 1],
+                    [1, 2, 1],]),
+                    ([(0, 0), (0, 1), (0, 2), (1, 2), (2, 2)],
+                     [(0, 0), (0, 1), (1, 1), (2, 1), (2, 2)],
+                     [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)],
+                     ),
+                    ([(0, 0), (0, 1), (1, 1), (1, 2), (2, 2)],
+                     [(0, 0), (1, 0), (1, 1), (2, 1), (2, 2)],
+                     [(0, 0), (1, 0), (1, 1), (1, 2), (2, 2)],
+                     ),
+                    )],
+        ids=["simple"]
+)           
+def test_is_backtrack(matrix, solutions, not_solutions):
+    (h, w) = matrix.shape
+    dp = DPArray((h, w))
+
+    dp[0, 0] = matrix[0, 0]
+    for i in range(h):
+        for j in range(w):
+            indices = []
+            elements = []
+            if i == 0 and j == 0:
+                continue
+
+            if i > 0:
+                indices.append((i - 1, j))
+                elements.append(dp[i - 1, j])
+
+            if j > 0:
+                indices.append((i, j - 1))
+                elements.append(dp[i, j - 1])
+
+            dp[i, j] = dp.max(indices, elements) + matrix[i, j]
+    
+    for solution in solutions:
+        assert dp.is_backtrack(solution)
+
+    for not_solution in not_solutions:
+        assert not dp.is_backtrack(not_solution)
+
 
 # Logger related tests #
 
