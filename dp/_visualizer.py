918d7d91517cd65f633d5e4d97a6d9e8747ff152
Aditya Prasad <aprasad4@usc.edu>
2023-11-04 10:32:07 -0700

    removed unnecessary comment

--- dp/_visualizer.py
+++ dp/_visualizer.py
@@ -378,7 +378,6 @@ def display(dp_arr,
     )
     def compare_input_and_frame(user_input, is_self_testing, current_frame, current_write):
         if is_self_testing and user_input != None:
-        # if is_self_testing and user_input != None and user_input.isdigit():
             next_frame = (current_frame + 1) % len(values)
             x,y = modded[next_frame][current_write]
             test = values[next_frame][x][y]
