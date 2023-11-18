65294ff1f9b71c32805ee0d93497fb897e1c40e4
Aditya Prasad <aprasad4@usc.edu>
2023-11-17 18:49:18 -0800

    removed comment for unneccessary buttons

--- dp/_visualizer.py
+++ dp/_visualizer.py
@@ -502,15 +502,6 @@ class Visualizer:
 
         max_timestep = len(self._graph_metadata[self._primary]["figure"].frames)
 
-        # button = html.Div(
-        #     [
-        #         dbc.Button("Button 1", class_name="me-md-2"),
-        #         dbc.Button("Button 2", class_name="me-md-2"),
-        #         dbc.Button("Button 3"),
-        #     ],
-        #     class_name="d-grid gap-2 d-md-flex justify-content-md-end",
-        # )
-
         questions = [
             "What is the next cell?",
             "What are its dependencies?",
