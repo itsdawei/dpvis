1eafbdb7684840e5e8926b8bde4e855759d7ffc2
Aditya Prasad <aprasad4@usc.edu>
2023-12-18 17:25:56 -0500

    linted

--- demos/knapsack.py
+++ demos/knapsack.py
@@ -6,8 +6,8 @@ from dp._visualizer import Visualizer
 # items is a list of items in the problem instance.
 def knapsack(items, capacity):
     # Initialize DPArray
-    OPT = DPArray((len(items)+1, capacity + 1), array_name="Knapsack")
-    DP_items = DPArray(shape=len(items), array_name = "Items", logger = OPT.logger)
+    OPT = DPArray((len(items) + 1, capacity + 1), array_name="Knapsack")
+    DP_items = DPArray(shape=len(items), array_name="Items", logger=OPT.logger)
     for i in range(len(items)):
         DP_items[i] = i
 
@@ -15,15 +15,15 @@ def knapsack(items, capacity):
     OPT[0, :] = 0
     OPT[:, 0] = 0
     # Recurrence: OPT(i, C) = max(OPT(i-1, C), OPT(i-1, C-i.space) + i.val)
-    for idx in range(len(items)+1):
+    for idx in range(len(items) + 1):
         for rem in range(capacity + 1):
             # Base case: 0 value if there are no items left or if there is no space.
             if idx == 0 or rem == 0:
                 continue
             # Normal case: There is an item to add and space remaining
-            item = items[idx-1]
+            item = items[idx - 1]
             if idx >= 1 and rem - item[0] >= 0:
-                _ = DP_items[idx-1]
+                _ = DP_items[idx - 1]
                 # OPT[idx, rem] = max(OPT[idx - 1, rem], OPT[idx - 1, rem-item[0]] + item[1])
                 indices = [
                     (idx - 1, rem),
@@ -37,7 +37,7 @@ def knapsack(items, capacity):
                 OPT.annotate(f"Comparing: max({indices[0]}, {indices[1]})")
             # Edge case: adding item is not possible
             elif idx >= 1 and rem - item[0] < 0:
-                _ = DP_items[idx-1]
+                _ = DP_items[idx - 1]
                 OPT[idx, rem] = OPT[idx - 1, rem]
                 OPT.annotate("Item does not fit in remaining capacity.")
 
@@ -52,7 +52,7 @@ def knapsack(items, capacity):
     # While the path is not fully constructed.
     while current[0] != 0 and current[1] != 0:
         i, rem = current
-        item = items[i-1]
+        item = items[i - 1]
 
         # Find the predecessor of current.
         # Case 1: adding item is possible and more optimal
@@ -87,10 +87,16 @@ elif idx >= 1 and rem - item[0] < 0:
 
     # Visualize with the items array
     visualizer = Visualizer()
-    visualizer.add_array(OPT, row_labels=row_labels, column_labels=column_labels, description=description)
+    visualizer.add_array(OPT,
+                         row_labels=row_labels,
+                         column_labels=column_labels,
+                         description=description)
     item_col_labels = [f"Item {i}: {item}" for i, item in enumerate(items)]
-    visualizer.add_array(DP_items, row_labels=" ", column_labels=item_col_labels)
+    visualizer.add_array(DP_items,
+                         row_labels=" ",
+                         column_labels=item_col_labels)
     visualizer.show()
+
 
 if __name__ == "__main__":
     items = [(2, 4), (4, 3), (7, 12), (5, 6), (13, 13)]
