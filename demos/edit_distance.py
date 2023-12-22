f2aa39e6482bd25a275029f19d97cbc866afa4b8
Aditya Prasad <aprasad4@usc.edu>
2023-12-21 22:56:08 -0500

    1d working

--- demos/edit_distance.py
+++ demos/edit_distance.py
@@ -54,17 +54,60 @@ str1 = "sunday"
 str2 = "saturday"
 
 
-dp_array = edit_distance(str1, str2, len(str1), len(str2))
-display(dp_array)
+# dp_array = edit_distance(str1, str2, len(str1), len(str2))
+# display(dp_array)
 
 """
 Question for student:
 1. How can we optimize the above DP to use a 2 x m array?
 2. How can we optimize the above DP to use just a 1d array?
-3. The above problem works when the cost of each action is 1, how
+3. The above solution works when the cost of each action is 1, how
    would you modify it if the cost of an add is alpha, the cost
    of a remove is beta, and the cost of a replace is tau?
 """
 
-def edit_distance_1d(str1, str2, alpha, beta, tau):
+
+str1 = "sunday"
+str2 = "saturday"
+
+# edit_distance_2xm(str1, str2, len(str1), len(str2))
+
+def edit_distance_1d(str1, str2, m, n):
+    curr = DPArray(n+1, array_name="OPT")
+    for j in range(n+1):
+        curr[j] = j
+    
+    previous = 0
+
+    for i in range(1, m+1):
+        # Store the current value at the beginning of the row.
+        previous = curr[0]
+        curr[0] = i
+
+        # Loop through the columns of the DPArray
+        for j in range(1, n+1):
+            # Store the current value in a temporary variable.
+            temp = curr[j]
+            
+            # Check if the characters at the current positions in str1 and str2 are the same
+            if str1[i-1] == str2[j-1]:
+                curr[j] = previous
+            else:
+                # Update the current cell with the minimum pf the three adjacent cells
+                curr[j] = 1 + min(previous, curr[j-1], curr[j])
+
+            # Update the previous variable with the temporary value
+            previous = temp
+    
+    # The value in the last cell represents the minimum number of operations
+    display(curr)
+    return curr[n]
+
+str1 = "sit"
+str2 = "kiit"
+
+ans = edit_distance_1d(str1, str2, len(str1), len(str2))
+print(ans)
+
+def edit_distance_different_costs(str1, str2, alpha, beta, tau):
     pass
