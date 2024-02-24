0af53a8fba581a8f1d2ce327699adb6aa4bbe93d
David Haolong Lee <dhlee@usc.edu>
2024-02-24 01:38:33 -0800

    Persistent Highlights; Square Cells; Fixing Knapsack (#59)
    * Fix hover text 1d
    
    * format
    
    * history
    
    * Add placeholder text for input box
    
    * merge
    
    * Persistent highlight and fix box shape
    
    * fix knapsack column labels
    
    * Escape \
    
    * change theme
    
    * Format
    
    * Make front-end look better
    
    * format
    
    * Format
    
    * Remove print

--- demos/knapsack.py
+++ demos/knapsack.py
@@ -72,10 +72,10 @@ def knapsack(items, capacity):
     OPT.add_traceback_path(path)
 
     # Define labels.
-    row_labels = [f"Item {i+1}: {item}" for i, item in enumerate(items)]
-    row_labels.insert(0, "No item")
-    column_labels = [f"Capacity {i}" for i in range(capacity + 1)]
-    description = "Recurrence: $OPT(i, C) = \max(OPT(i-1, C), OPT(i-1, C-c(i)) + v(i))$"
+    row_labels = ["No item"]
+    row_labels += [f"Item {i}: {item}" for i, item in enumerate(items, start=1)]
+    column_labels = [f"Capacity {i}" for i in range(max_capacity + 1)]
+    description = "Recurrence: $OPT(i, C) = \\max(OPT(i-1, C), OPT(i-1, C-c(i)) + v(i))$"
 
     # Visualize with the items array
     visualizer = Visualizer()
@@ -83,7 +83,7 @@ def knapsack(items, capacity):
                          row_labels=row_labels,
                          column_labels=column_labels,
                          description=description)
-    item_col_labels = [f"Item {i}: {item}" for i, item in enumerate(items)]
+    item_col_labels = [f"Item {i}: {item}" for i, item in enumerate(items, start=1)]
     visualizer.add_array(DP_items,
                          row_labels=" ",
                          column_labels=item_col_labels)
@@ -92,5 +92,5 @@ def knapsack(items, capacity):
 
 if __name__ == "__main__":
     items = [(2, 4), (4, 3), (7, 12), (5, 6), (13, 13)]
-    capacity = 14
-    knapsack(items, capacity)
+    max_capacity = 14
+    knapsack(items, max_capacity)
